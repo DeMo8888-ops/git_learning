106f4f2b1cdefe473b8c6e19eca4602621c9ac26
DeMo8888-ops <tggalmukova@gmail.com>
2025-07-07 23:30:31 +0300

    Task-1 - Реализовано умножение

--- calculator1.py
+++ calculator1.py
@@ -1,7 +1,12 @@
+
 class Calculator:
     last_res = None
 
     def sum(self, n1, n2):
+        self.last_res = n1 + n2
+        return n1 + n2
+
+    def multiply(self, n1, n2):
         self.last_res = n1 + n2
         return n1 + n2
 
